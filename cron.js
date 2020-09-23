000ba8158d7432e39061705ef45fade5ef9bd64a
fmaduakor <felix.maduakor@mlcrypt.de>
2020-09-23 11:29:08 -0400

    cronjob every 12 hours

--- cron.js
+++ cron.js
@@ -4,7 +4,7 @@ var SunnyPortal = require('./private/sunnyportal');
 const spDailyKWH = require("./controllers/spDailyKWH_controller");
 
 
-cron.schedule("*/2 * * * *", function() {
+cron.schedule("0 */12 * * *", function() {
     console.log(new Date().toString() + " | Gathering SunnyPortal Data for: " + process.env.SPusername + "; Plant: " + process.env.SPplantID);
     var sunnyPortal = new SunnyPortal(process.env.SPusername, process.env.SPpassword, process.env.SPplantID);
     sunnyPortal.init().then(function() {
