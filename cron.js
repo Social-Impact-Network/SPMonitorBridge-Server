381fedf5870eb100059e0693f74ecbd3710d2cf8
fmaduakor <felix.maduakor@mlcrypt.de>
2020-09-23 05:46:39 -0400

    change from credentials to dotenv

--- cron.js
+++ cron.js
@@ -1,11 +1,12 @@
 const cron = require("node-cron");
 var SunnyPortal = require('./private/sunnyportal');
-const credentials = require('./private/credentials');
+
 const spDailyKWH = require("./controllers/spDailyKWH_controller");
 
 
 cron.schedule("*/1 * * * *", function() {
 console.log("test");
+console.log(process.env);
 
 /*    console.log("---------------------");
     console.log(new Date().toString() + " | Gathering SunnyPortal Data for: " + credentials.username + "; Plant: " + credentials.plantID);
