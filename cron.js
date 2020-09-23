2106be7b56bb5fcdda2dc18b5b82448b611ad6ad
fmaduakor <felix.maduakor@mlcrypt.de>
2020-09-23 10:36:36 -0400

    cronjob change

--- cron.js
+++ cron.js
@@ -4,11 +4,10 @@ var SunnyPortal = require('./private/sunnyportal');
 const spDailyKWH = require("./controllers/spDailyKWH_controller");
 
 
-cron.schedule("*/1 * * * *", function() {
+cron.schedule("*/2 * * * *", function() {
     console.log(new Date().toString() + " | Gathering SunnyPortal Data for: " + process.env.SPusername + "; Plant: " + process.env.SPplantID);
-
-    /*var sunnyPortal = new SunnyPortal(process.env.SPusername, process.env.SPpassword, process.env.SPplantID);
+    var sunnyPortal = new SunnyPortal(process.env.SPusername, process.env.SPpassword, process.env.SPplantID);
     sunnyPortal.init().then(function() {
       spDailyKWH.create(process.env.SPplantID,sunnyPortal.PVyesterday[1],sunnyPortal.PVyesterday[0]);
-    })*/
+    })
   });
